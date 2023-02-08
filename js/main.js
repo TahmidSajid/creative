bdbaf0a470f41faa358ec18fc04405d0aad1377d
TahmidSajid <tahmidkhansajid@gmail.com>
2023-02-09 04:18:18 +0600

    done

---
+++ js/main.js
@@ -0,0 +1,23 @@
+var mixer = mixitup('.main_mix');
+$('.team_sec').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: true,
+    centerMode: true,
+    centerPadding: '0',
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
+  $('.feed_sec').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: true,
+    centerMode: true,
+    centerPadding: '0',
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
